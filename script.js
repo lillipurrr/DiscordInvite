f573b1345575199288ef8ed07aa7e17d00a62fb0
lillipurrr <lillipurrr@gmail.com>
2025-01-05 05:31:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-widgetURL = "https://discord.com/api/guilds/1234/widget.json";
+widgetURL = "https://discord.com/api/guilds/1254416810997186632/widget.json";
 
 function card(e, val, rand) {
   e += '<div class="card" style="--duration:' + rand + 's">';
